6e935fd6c408638e758389f85b3e6a53fd63b848
Crowdbotics <engineering@crowdbotics.com>
2023-07-03 00:25:16 +0000

    Initial commit.

---
+++ backend/testapiautomation90_96800/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for testapiautomation90_96800 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testapiautomation90_96800.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "testapiautomation90-96800",
+  url: "https://testapiautomation90-96800.botics.co",
+  api: "https://testapiautomation90-96800.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
